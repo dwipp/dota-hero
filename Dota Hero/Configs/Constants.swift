5a77bf4259a5871c7e778333d25d212647e01261
Dwi Permana Putra <dwi.putra@icloud.com>
2020-12-04 08:43:31 +0800

    API configs

---
+++ Dota Hero/Configs/Constants.swift
@@ -0,0 +1,33 @@
+//
+//  Constants.swift
+//  Dota Hero
+//
+//  Created by Dwi Putra on 04/12/20.
+//
+
+import Foundation
+import Alamofire
+
+struct Constants {
+    struct ProductionServer {
+        static let baseURL = "https://api.opendota.com/api"
+    }
+}
+
+enum HTTPHeaderField:String {
+    case authentication = "Authorization"
+    case contentType = "Content-Type"
+    case acceptType = "Accept"
+    case acceptEncoding = "Accept-Encoding"
+    case string = "String"
+}
+
+enum ContentType:String {
+    case  json = "Application/json"
+    case formEncode = "application/x-www-form-urlencoded"
+}
+
+enum RequestParams {
+    case body(_:Parameters)
+    case url(_:Parameters)
+}
