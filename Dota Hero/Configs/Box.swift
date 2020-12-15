6428b998312ef45d5b6d23d9958cc6801e2bfa08
Dwi Permana Putra <dwi.putra@icloud.com>
2020-12-15 18:24:32 +0800

    Box for binding

---
+++ Dota Hero/Configs/Box.swift
@@ -0,0 +1,27 @@
+//
+//  Box.swift
+//  Dota Hero
+//
+//  Created by Dwi Putra on 13/12/20.
+//
+
+import Foundation
+
+final class Box<T> {
+    typealias Listener = (T) -> Void
+    var listener: Listener?
+    var value: T {
+        didSet {
+            listener?(value)
+        }
+    }
+    
+    init(_ value:T) {
+        self.value = value
+    }
+    
+    func bind(_ listener: Listener?) {
+        self.listener = listener
+        listener?(value)
+    }
+}
